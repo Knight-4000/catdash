0d9a6ec278beb6919d8708d5fad5825bf01379d5
unknown <yorksprinters@gmail.com>
2023-10-09 08:04:53 -0700

    Updated hamburger button and navlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-blue-500 py-2 text-neutral-content">
+    <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
         <div className='flex gap-x-2 sm:gap-x-8 items-center'>
           <p className='text-xs sm:text-sm'>Hello, George</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,48 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
-import { FaBarsStaggered } from 'react-icons/fa6';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [theme, setTheme] = useState(false);
+
+    const handleTheme = () => {
+        setTheme(!theme);
+    }
   return (
-    <nav className="bg-blue-300">
+    <nav className="bg-base-200">
       <div className="navbar align-element">
         <div className="navbar-start">
             <NavLink to='/' className='hidden lg:flex btn btn-primary text-lg items-center'>Cat Dash</NavLink>
             <div className='dropdown'>
             <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-              <FaBarsStaggered className='h-6 w-6' />
+                <label className="btn btn-circle swap swap-rotate">
+                    <input type="checkbox" />
+                    <AiOutlineCloseCircle className='swap-on h-8 w-8' />
+                    <GiHamburgerMenu className='swap-off h-8 w-8' />
+                </label>
             </label>
             <ul
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'
             >
-              <p>Home</p>
+              <NavLinks />
             </ul>
           </div>
         </div>
             <div className='navbar-center hidden lg:flex'>
-                <ul className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'>
-                    Navlinks
+                <ul className='menu menu-horizontal'>
+                   <NavLinks />
                 </ul>
             </div>
             <div className='navbar-end'>
+                <label className='swap swap-rotate'>
+                    <input type='checkbox' onChange={handleTheme} />
+                    <BsSunFill className='swap-on h-4 w-4' />
+                    <BsMoonFill className='swap-off h-4 w-4' />
+                </label>
                 <NavLink to ='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
                     <div className='indicator'>
                         <BsCart3 className='h-6 w-6' />
